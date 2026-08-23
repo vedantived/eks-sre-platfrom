dab64d141d71ae41fa055f11c7705b1e53782973
vedanti mardhekar <vedantim98@gmail.com>
2026-08-23 19:47:52 +0530

    Add VPC and subnet resources in main.tf

---
+++ module/vpc/main.tf
@@ -0,0 +1,136 @@
+resource "aws_vpc" "this" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name = "${var.project_name}-vpc"
+  }
+}
+
+// Avaibility zone (AZ)
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+// Public subnet
+
+resource "aws_subnet" "public" {
+  count = length(var.public_subnet_cidrs)
+
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = var.public_subnet_cidrs[count.index]
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  map_public_ip_on_launch = true
+
+tags = merge(
+  var.common_tags,
+  {
+    Name = "${var.project_name}-public-${count.index + 1}"
+
+    "kubernetes.io/role/elb" = "1"                         //Used by Kubernetes → creates public load balancer
+    "kubernetes.io/cluster/eks-zero-trust" = "shared"         //Links subnet to EKS cluster
+  }
+)
+}
+
+// Private Subnet
+
+resource "aws_subnet" "private_app" {
+  count = length(var.private_app_subnet_cidrs)
+
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = var.private_app_subnet_cidrs[count.index]
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+
+tags = merge(
+  var.common_tags,
+  {
+    Name = "${var.project_name}-private-app-${count.index + 1}"
+
+    "kubernetes.io/role/internal-elb" = "1"
+    "kubernetes.io/cluster/sre_cluster" = "shared"
+  }
+)
+}
+
+
+// Internet Gateway
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    Name = "${var.project_name}-igw"
+  }
+}
+
+// Elastic IPs for NAT.
+
+resource "aws_eip" "nat" {
+  domain = "vpc"
+
+  tags = {
+    Name = "${var.project_name}-nat-eip"
+  }
+}
+
+//NAT Gateways (Per AZ)
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public[0].id
+
+  tags = {
+    Name = "${var.project_name}-nat"
+  }
+
+  depends_on = [aws_internet_gateway.igw]        //Ensure IGW created first
+}
+
+// Public Route Table
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "${var.project_name}-public-rt"
+  }
+}
+
+// Associate Public Subnets
+
+resource "aws_route_table_association" "public_assoc" {
+  count          = length(var.public_subnet_cidrs)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
+// Private App Route Tables (Per AZ)
+
+resource "aws_route_table" "private_app" {
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+
+  tags = {
+    Name = "${var.project_name}-private-app-rt"
+  }
+}
+
+//Associate Private App Subnets
+
+resource "aws_route_table_association" "private_app_assoc" {
+  count          = length(var.private_app_subnet_cidrs)
+  subnet_id      = aws_subnet.private_app[count.index].id
+  route_table_id = aws_route_table.private_app.id
+}
