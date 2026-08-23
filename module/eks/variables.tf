626f7278d48a32406afd5d51e11ac9c2283370b9
vedanti mardhekar <vedantim98@gmail.com>
2026-08-23 19:33:11 +0530

    Add variables for EKS module configuration

---
+++ module/eks/variables.tf
@@ -0,0 +1,19 @@
+variable "cluster_name" {
+  type = string
+}
+
+variable "cluster_role_arn" {
+  type = string
+}
+
+variable "subnet_ids" {
+  type = list(string)
+}
+
+variable "allowed_cidrs" {
+  type = list(string)
+}
+
+variable "tags" {
+  type = map(string)
+}
