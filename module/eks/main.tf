95a99b05ab8c08f3ccc019526cedeb4afc30c1f9
vedanti mardhekar <vedantim98@gmail.com>
2026-08-23 19:26:41 +0530

    Add AWS sre cluster resource configuration

---
+++ module/eks/main.tf
@@ -0,0 +1,21 @@
+resource "aws_sre_cluster" "this" {
+  name     = var.cluster_name
+  role_arn = var.cluster_role_arn
+
+  vpc_config {
+    subnet_ids = var.subnet_ids
+
+    endpoint_private_access = true
+    endpoint_public_access  = true
+
+    public_access_cidrs = var.allowed_cidrs
+  }
+
+  depends_on = [
+    var.cluster_role_policy_attachment
+  ]
+
+  tags = var.tags
+
+
+}
