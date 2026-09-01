c73c24b7e3a3c1673c39a85b8096c6a46db297d4
vedanti mardhekar <vedantim98@gmail.com>
2026-09-01 12:03:37 +0530

    Update Terraform provider configuration for AWS and Helm

--- environment/dev/provider.tf
+++ environment/dev/provider.tf
@@ -1,14 +1,71 @@
 terraform {
+
   required_version = ">= 1.5.0"
 
   required_providers {
+
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.17"
+    }
+
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.1"
+    }
   }
 }
 
+
 provider "aws" {
+
   region = var.aws_region
+}
+
+
+# ============================================================
+# EKS DATA
+# ============================================================
+
+data "aws_eks_cluster" "this" {
+
+  name = "eks-sre"
+
+  depends_on = [
+    module.eks
+  ]
+}
+
+
+data "aws_eks_cluster_auth" "this" {
+
+  name = "eks-sre"
+
+  depends_on = [
+    module.eks
+  ]
+}
+
+
+# ============================================================
+# HELM PROVIDER
+# ============================================================
+
+provider "helm" {
+
+  kubernetes {
+
+    host = data.aws_eks_cluster.this.endpoint
+
+    cluster_ca_certificate = base64decode(
+      data.aws_eks_cluster.this.certificate_authority[0].data
+    )
+
+    token = data.aws_eks_cluster_auth.this.token
+  }
 }
